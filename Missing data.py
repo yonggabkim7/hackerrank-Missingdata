8b9cfaf5606cef63e80e2c0fa5af6510eae03ec0
Yonggab Kim <kim3233@purdue.edu>
2020-09-30 16:06:39 -0400

    Added code

---
+++ Missing data.py
@@ -0,0 +1,34 @@
+import fileinput
+
+#####Extract the price value
+i = -1
+prices = []
+for line in fileinput.input():
+    if i == -1:
+        n = int(line)
+        i = i + 1
+    else:
+        prices.append(line)
+
+
+def calcMissing(readings):
+    def isfloat(value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
+    for n in range(len(readings)):
+        a = readings[n]
+        a1 = a.split()[2]
+        a2 = [char for char in a1]
+        if 'M' in a2:
+            k = []
+            for n2 in range(4):
+                if (n - 2 + n2) < len(readings) and isfloat(readings[n - 2 + n2].split()[2]):
+                    k.append(float(readings[n - 2 + n2].split()[2]))
+            print(sum(k) / len(k))
+
+
+calcMissing(prices)
